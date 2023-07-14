b9125c4f142127b80c4e00b61dbf19d3e14ed858
roodhouse <rughjm@gmail.com>
2023-07-14 09:26:06 -0500

    output style

--- src/components/Output.jsx
+++ src/components/Output.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 function Output() {
   return (
     <>
-      <div id="outputContainer" className='bg-backgroundScreenOne pr-6 pt-[29px] pb-[22px] text-right text-white font-leagueSpartan text-[40px] font-bold tracking-[-0.667px] rounded-[10px]'>
+      <div id="outputContainer" className='pr-6 pt-[29px] pb-[22px] text-right font-leagueSpartan text-[40px] font-bold tracking-[-0.667px] rounded-[10px]'>
         <p>399,981</p>
       </div>
     </>
